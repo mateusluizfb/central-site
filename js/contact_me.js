f0e829a3f8e9ce173f1cf7e5ba32aceaa784d701
Mateus Luiz Freitas Barros <mateuslfb@hotmail.com>
2017-01-14 00:58:27 +0000

    Uploading new contact bg

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,17 +8,17 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
+            var name = $("input#_name").val();
+            var email = $("input#_email").val();
+            var phone = $("input#_phone").val();
+            var message = $("textarea#_message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://formspree.io/mateuslfreitasb@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
@@ -26,6 +26,7 @@ $(function() {
                     email: email,
                     message: message
                 },
+                dataType: "json",
                 cache: false,
                 success: function() {
                     // Success message
